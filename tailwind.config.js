4b5b2bd4e9a4fd54d78938ab9d3e54f1eae3add5
Farad Alam <farad.dev@gmail.com>
2025-03-01 16:36:30 +0600

    topbar added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,12 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "gradient-to-r": "linear-gradient(to right, var(--tw-gradient-stops))",
+      },
+    },
   },
   plugins: [],
-}
+};
 
